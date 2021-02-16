6998459ccef185ca6fbb30da3107dc4fcff771b7
tathamet <aaantaeus@gmail.com>
2021-02-17 01:33:15 +0300

    better locators

--- src/test/java/Automation.java
+++ src/test/java/Automation.java
@@ -35,28 +35,28 @@ public class Automation {
         driver.get("https://help.salesforce.com");
 
 //2) В инпут на странице написать outloook for salesforce  // в слове "outloook" опечатка, поэтому не удавалось сразу найти Quick Starts
-        findElement(By.xpath("//tds-header-search/div[@id='tds-header-search']/div[1]/div[1]/div[2]/div[1]/input[1]")).sendKeys("outlook for salesforce");
+        findElement(By.xpath("//input[@placeholder='Search Knowledge articles, best practices, and more...']")).sendKeys("outlook for salesforce");
 
 //3) Нажать на иконку лупы
-        findElement(By.xpath("//tds-header-search/div[@id='tds-header-search']/div[1]/span[1]/*[1]")).click();
+        findElement(By.cssSelector(".tds-search-input__icon")).click();
         waitingForElement(By.xpath("//a[contains(text(),'Help us Improve your results in 1 minute or less.')]"));
 
 //4) На следующей странице в блоке Content Type выбрать Quick Starts
-        findElement(By.xpath("//body/div[1]/div[1]/div[1]/span[1]/help-search[1]/div[1]/search-results[1]/div[1]/div[2]/div[1]/div[4]/div[3]/div[2]")).click();
+        findElement(By.xpath("//div[@aria-label='Expand Content Type facet']")).click();
         waitingForElement(By.xpath("//span[contains(text(),'Quick Starts')]"));
         findElement(By.xpath("//span[contains(text(),'Quick Starts')]")).click();
 
 //5) Кликнуть на первый результат на странице
         waitingForElement(By.xpath("//a[contains(text(),'Getting Started with Sales Cloud Einstein')]"));
-        findElement(By.xpath("//body/div[1]/div[1]/div[1]/span[1]/help-search[1]/div[1]/search-results[1]/div[1]/div[2]/div[2]/div[1]/div[9]/div[2]/div[1]/div[1]/a[1]")).click();
+        findElement(By.xpath("//a[@class='CoveoResultLink'][1]")).click();
 
 //6) На следующей странице проверить, что блок Related Resources присутствует
-        waitingForElement(By.linkText("Contact Support"));
+        waitingForElement(By.cssSelector(".tdx-button-brand.contact-us-button.hide-in-console"));
 //        Thread.sleep(2000);
         if (findElement(By.xpath("//div[@id='relatedresources_articleview']")).isDisplayed()) {
-            System.out.println("Блок Related Resources присутствует");
+            System.out.println("Related Resources is displayed");
         } else {
-            System.out.println("Блок Related Resources отсутствует");
+            System.out.println("Related Resources is not displayed");
         }
 
 //7) Кликнуть на первый результат в этом блоке
@@ -67,9 +67,9 @@ public class Automation {
         List<WebElement> dynamicElements = driver.findElements(By.xpath("//div/error-block"));
         if (dynamicElements.size() != 0) {
             String errors = findElement(By.xpath("//div/error-block")).getAttribute("error-code");
-            System.out.println("Страница показывает ошибку: " + errors);
+            System.out.println("The page shows error: " + errors);
         } else {
-            System.out.println("Страница не показывает ошибку 404");
+            System.out.println("The page doesn't show Error 404");
         }
 
         driver.quit();
